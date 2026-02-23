58676c021f65383ebef552115d915ae41abf04b6
leska33 <alessiamin94@gmail.com>
2026-02-23 18:34:36 +0300

    Initial commit

--- src/main/java/com/example/student/mapper/StudentMapper.java
+++ src/main/java/com/example/student/mapper/StudentMapper.java
@@ -3,11 +3,15 @@ package com.example.student.mapper;
 import com.example.student.dto.StudentResponseDto;
 import com.example.student.entity.Student;
 
-public class StudentMapper {
+public final class StudentMapper {
+
+    private StudentMapper() {
+    }
     public static StudentResponseDto toDto(Student student) {
         if (student == null) {
             return null;
         }
-        return new StudentResponseDto(student.getStudentId(), student.getFullName(), student.getGroupNumber(), student.getAttendanceCount(), student.getAverageGrade());
+        return new StudentResponseDto(student.getStudentId(), student.getFullName(), student.getGroupNumber(), student.getAttendanceCount(), student.getAverageGrade()
+        );
     }
 }
