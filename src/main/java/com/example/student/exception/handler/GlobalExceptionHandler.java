81b15b251217426f5fe80464b3374178c3612c4f
leska33 <alessiamin94@gmail.com>
2026-03-29 20:37:58 +0300

    Initial commit

--- src/main/java/com/example/student/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/example/student/exception/handler/GlobalExceptionHandler.java
@@ -6,7 +6,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.LocalDateTime;
 
